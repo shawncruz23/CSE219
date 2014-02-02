9ab85fa6210770699eb7a88e9e5d33530cec7f56
Shawn Cruz <shawn.cruz@stonybrook.edu>
2014-02-02 14:24:46 -0500

    HelloWorld project commit

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello Word!");
+       System.out.println("Hello World!");
     }
     
 }
